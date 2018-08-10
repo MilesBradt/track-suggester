c579681392e843bafc44086d6e6e45a5c229f5bd
MilesBradt <smbradtmichael@gmail.com>
2018-08-10 16:22:37 -0700

    add README

---
+++ track-suggester/README.md
@@ -0,0 +1,39 @@
+# _Track Suggester_
+
+#### _Super short questionnaire to pair a user with a programming language they may like, Aug 10th 2018_
+
+#### By _**Miles Bradt**_
+
+## Description
+
+_Uses basic javascript to pull user information based off questions answered to provide resources to learning a programming language they might be interested in_
+
+## Setup/Installation Requirements
+
+* _Clone repository at: https://github.com/MilesBradt/track-suggester.git_
+* _View webpage on: _
+
+## Known Bugs
+
+_No known bugs so far, but there is a lack of content_
+
+## Support and contact details
+
+_Best way to reach me is my person email via smbradtmichael@gmail.com_
+
+## Technologies Used
+
+* _JavaScript/jQuery_
+* _CSS/Bootstrap_
+* _HTML_
+
+## Sources
+
+* _Roboto font from: https://fonts.googleapis.com/css?family=Roboto_
+* _Epicodus for descriptions on each programming language: https://www.epicodus.com/blog/what-programming-language-should-i-learn_
+
+### License
+
+**_N/A_**
+
+Copyright (c) 2018 **_Miles Bradt_**
--- track-suggester/js/scripts.js
+++ track-suggester/js/scripts.js
@@ -1,57 +1,54 @@
 $(document).ready(function() {
+  $("button#add").click(function(event) {
+    event.preventDefault();
+    var csharpOk = 0
+    var cssOk = 0
+    var rubyOk = 0
+    var checked1 = $("input:radio[name=first]:checked").val();
+    var checked2 = $("input:radio[name=second]:checked").val();
 
-    $("button#add").click(function(event) {
-      event.preventDefault();
-      var csharpOk = 0
-      var cssOk = 0
-      var rubyOk = 0
-      var checked1 = $("input:radio[name=first]:checked").val();
-      var checked2 = $("input:radio[name=second]:checked").val();
+    if (checked1 === "csharpYes") {
+      csharpOk ++
+    }
 
-      if (checked1 === "csharpYes") {
-        csharpOk ++
-      }
+    else if (checked1 === "cssYes") {
+      cssOk += 3
+    }
 
-      else if (checked1 === "cssYes") {
-        cssOk += 3
-      }
+    if (checked2 === "rubyYes") {
+      rubyOk += 2
+    }
 
-      if (checked2 === "rubyYes") {
-        rubyOk += 2
-      }
+    else if (checked2 === "csharpYes") {
+      csharpOk += 2
+    }
 
-      else if (checked2 === "csharpYes") {
-        csharpOk += 2
-      }
+    if (csharpOk === 0 && cssOk === 0 && rubyOk === 0) {
+      alert("Not all answers were filled out");
+    }
 
-      if (csharpOk === 0 && cssOk === 0 && rubyOk === 0) {
-        alert("Not all answers were filled out");
-      }
+    else if (csharpOk > cssOk && csharpOk > rubyOk) {
+      $('.csharpCheck').show();
+      $('.cssCheck').hide();
+      $('.rubyCheck').hide();
+    }
 
-      else if (csharpOk > cssOk && csharpOk > rubyOk) {
-        $('.csharpCheck').show();
-        $('.cssCheck').hide();
-        $('.rubyCheck').hide();
-      }
+    else if (cssOk > csharpOk && cssOk > rubyOk) {
 
-      else if (cssOk > csharpOk && cssOk > rubyOk) {
+      $('.cssCheck').show();
+      $('.csharpCheck').hide();
+      $('.rubyCheck').hide();
+    }
 
-        $('.cssCheck').show();
-        $('.csharpCheck').hide();
-        $('.rubyCheck').hide();
-      }
+    else if (rubyOk > csharpOk && rubyOk > cssOk) {
+      $('.rubyCheck').show();
+      $('.cssCheck').hide();
+      $('.csharpCheck').hide();
+    }
 
-      else if (rubyOk > csharpOk && rubyOk > cssOk) {
-        $('.rubyCheck').show();
-        $('.cssCheck').hide();
-        $('.csharpCheck').hide();
-      }
-
-
-      console.log(csharpOk);
-      console.log(cssOk);
-      console.log(rubyOk);
-      $('input[type="radio"]').prop('checked', false);
+    console.log(csharpOk);
+    console.log(cssOk);
+    console.log(rubyOk);
+    $('input[type="radio"]').prop('checked', false);
   });
-
 });
